ecc39662fcbdb82c3715586ab8cba246a9094512
Ved Mitra Verma <vedmitraverma24@gmail.com>
2026-04-08 18:15:47 +0530

    making cmd lokking in root

--- user/sh.c
+++ user/sh.c
@@ -77,6 +77,12 @@ runcmd(struct cmd *cmd)
     if(ecmd->argv[0] == 0)
       exit(1);
     exec(ecmd->argv[0], ecmd->argv);
+    if(ecmd->argv[0][0] != '/') { // to always look for commands in root directory
+        char path[100];
+        path[0] = '/';
+        strcpy(path + 1, ecmd->argv[0]);
+        exec(path, ecmd->argv);
+    }
     fprintf(2, "exec %s failed\n", ecmd->argv[0]);
     break;
 
